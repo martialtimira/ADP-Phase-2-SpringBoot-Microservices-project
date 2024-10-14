45c0a277be004642797edd747c158300b4299b05
martialtimira <marti.altimira.cebrian@adp.com>
2024-10-14 12:22:49 +0200

    removed Mapping parenthesis

--- back-end/src/main/java/com/example/back_end/api/CustomerAPI.java
+++ back-end/src/main/java/com/example/back_end/api/CustomerAPI.java
@@ -1,7 +1,6 @@
 package com.example.back_end.api;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +24,7 @@ public class CustomerAPI {
         customerList.add(c3);
     }
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<?> getCustomers() {
         return ResponseEntity.ok(customerList);
     }
